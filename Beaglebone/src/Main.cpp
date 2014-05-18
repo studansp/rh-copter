2b9328e8782acd419770ec0a48011c82b1e88ae6
studansp <pstudans@gmail.com>
2014-05-18 19:19:23 -0400

    Test commit

---
+++ Beaglebone/src/Main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: May 18, 2014
+ *      Author: phillip
+ */
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello world";
+
+	return 0;
+}
